aa661bcb06760a8bc34350abc65874cb839b96fd
Galambur <59735586+Galambur@users.noreply.github.com>
2022-11-29 15:40:24 +0100

    Ajout des phrases de Merlin

--- ArchiRPG/RencontreMerlin.cs
+++ ArchiRPG/RencontreMerlin.cs
@@ -13,12 +13,25 @@ namespace ArchiRPG
 
 			var randomCustom = new RandomLibrary();
 			var pourcentHeal = randomCustom.getPourcentage(1, 100);
+			
 			Console.WriteLine("Merlin va vous rendre " + pourcentHeal + "% de votre vie !");
 
 			var healing = new Healing();
 			joueur = healing.RestorePv(joueur, pourcentHeal);
+			PhraseMerlin(pourcentHeal);
 
 			return joueur;
+		}
+		
+		private void PhraseMerlin(int pourcent){
+			if(pourcent < 10)
+				Console.WriteLine("La boutique est fermée, adressez-vous à la concurrence.");
+			else if(pourcent >= 10 && pourcent < 40)
+				Console.WriteLine("Moi j'ai vomi deux fois mais c'est moi qu'ai ramassé  !");
+			else if( pourcent >= 40 && pourcent < 85)
+				Console.WriteLine("Moi, je dis que c'est magique à cause des merdes qui pendouillent, mais ça se trouve, c'est pas ça...");
+			else if(pourcent >= 85)
+				Console.WriteLine("Attendez, je veux être là quand vous lui mettez une chasse !");
 		}
 	}
 }
