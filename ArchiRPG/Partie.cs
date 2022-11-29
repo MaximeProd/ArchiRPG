88168fae1f23ae7b8b5f37b78f4b26cdd1a798bc
Galambur <59735586+Galambur@users.noreply.github.com>
2022-11-29 12:07:24 +0100

    Update Partie.cs
    réorganisé + commence le déroulement de la partie

--- ArchiRPG/Partie.cs
+++ ArchiRPG/Partie.cs
@@ -11,24 +11,34 @@ namespace ArchiRPG
 		private Joueur joueur { get; set; }
 
 		public void StartPartie(){
-			joueur = new Joueur()
-			{
-				Niveau = 1,
-                Force = 15,
-                Armure = 15,
-				PointDeVie = 30,
-				PointDeVieMax= 30
-			};
 			Console.WriteLine("Bonjour et bienvenue dans LE rpg !" + 
 								"\nNous allons commencer par affecter vos 12 points de compétences.");
+			// début de la partie
+			InitialiserPerso();
 			CreationPerso();
-
 			joueur.afficherStats();
+			
+			//lancement des 20 tours
+			for (int i = 0; i < 20; i++){
+				var tour = new Tour();
+				// TODO implementer le metier du tour
+			}
 		}
 
         private void CreationPerso(){
 			var attribution = new AttributionCompetence();
 			attribution.AttribuerCompetence(joueur, 12);
         }
+		
+	private void InitialiserPerso(){
+		joueur = new Joueur()
+		{
+			Niveau = 1,
+			Force = 15,
+			Armure = 15,
+			PointDeVie = 30,
+			PointDeVieMax= 30
+		};
+	}
     }
 }
